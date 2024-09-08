467082a8c53cf9ec4b59a19805ecc61aca484f9d
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-09-08 07:38:56 +0530

    styles: add sass to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import '@/styles/main.scss';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
