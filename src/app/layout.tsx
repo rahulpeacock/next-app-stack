2077c501017df1e06bcecb26706abca9190a448d
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-09-08 07:55:31 +0530

    feat: add darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/client/providers/theme-provider';
 import '@/styles/main.scss';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className}`}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
